56bf0a52fad34f429e807cf1e00a241036a2719a
EmilioLink <emiliolopez700@gmail.com>
2020-03-25 12:22:59 -0600

    Creacion del JFrame para el Inventario, creacion de la clase para control de Inventario

---
+++ FarmaciaDespertad/src/Clases/Inventario.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author emili
+ */
+public class Inventario {
+    static int id = 0;
+    //String marca, tipo;
+    //Double precio;
+   // int cantidadDisponible;
+    //Date fechaVencimiento,fechaIngreso;
+    static String[][] inventario;
+
+    public Inventario() {
+        inventario = new String[100][6];
+    }
+
+    public void setNewItem(String marca,String tipo,Double precio,int cantidad,Date fechaVencimiento){
+        int newid = getNextID();
+        inventario[newid][0] = String.valueOf(newid);
+        inventario[newid][1] = marca;
+        inventario[newid][2] = tipo;
+        inventario[newid][3] = String.valueOf(precio);
+        inventario[newid][4] = String.valueOf(cantidad);
+        inventario[newid][5] = String.valueOf(fechaVencimiento);
+    }
+    
+    public int getNextID(){
+        return id++;
+    }
+    
+    
+}
