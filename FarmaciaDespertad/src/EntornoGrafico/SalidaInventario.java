83c0e458ef8254f926d325c1dd790746cebcdb4f
EmilioLink <emiliolopez700@gmail.com>
2020-03-26 11:11:16 -0600

    Añadir logica de Busqueda de Inventario

--- FarmaciaDespertad/src/EntornoGrafico/SalidaInventario.java
+++ FarmaciaDespertad/src/EntornoGrafico/SalidaInventario.java
@@ -5,17 +5,22 @@
  */
 package EntornoGrafico;
 
+import Clases.Inventario;
+import java.util.Vector;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author emili
  */
 public class SalidaInventario extends javax.swing.JInternalFrame {
-
+     Inventario inv;
     /**
      * Creates new form SalidaInventario
      */
     public SalidaInventario() {
         initComponents();
+        inv = Home.inventario;
     }
 
     /**
@@ -27,6 +32,11 @@ public class SalidaInventario extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblGeneral = new javax.swing.JTable();
+        jButton1 = new javax.swing.JButton();
+
         setClosable(true);
         setIconifiable(true);
         setMaximizable(true);
@@ -35,21 +45,90 @@ public class SalidaInventario extends javax.swing.JInternalFrame {
         setToolTipText("null");
         setPreferredSize(new java.awt.Dimension(1371, 851));
 
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel2.setText("Salida de Inventario");
+
+        tblGeneral.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Id", "Marca / Fabricante", "Tipo", "Precio", "Fecha de Vencimiento", "Cantidad que ingreso"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tblGeneral);
+
+        jButton1.setText("Consulta de Inventario");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1032, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(439, 439, 439)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1)
+                            .addComponent(jLabel2))
+                        .addGap(0, 752, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 665, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addGap(28, 28, 28)
+                .addComponent(jButton1)
+                .addGap(27, 27, 27)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(227, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String[][] invtArray = inv.getArray();
+        
+        if(invtArray != null){
+            for(int i=0; i<invtArray.length; i++) {       
+              DefaultTableModel model = (DefaultTableModel) tblGeneral.getModel();
+
+                Vector row = new Vector();
+                row.add(invtArray[i][0]);
+                row.add(invtArray[i][1]);
+                row.add(invtArray[i][2]);
+                row.add(invtArray[i][3]);
+                row.add(invtArray[i][5]);
+                row.add(invtArray[i][4]);
+                model.addRow(row);        
+            }       
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblGeneral;
     // End of variables declaration//GEN-END:variables
 }
